9571be56f35d30131f3aa83aad85026353c8463a
Ben Hoover <24350185+bhoov@users.noreply.github.com>
2025-07-08 11:11:46 -0400

    having a hard time previewing suddenly

--- scripts/prep_colab_nbs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nbdev_qmd_to_ipynb nbs/tutorial nbs/_tutorial_ipynbs --copy_other_files False
--- scripts/prep_deploy.sh
+++ scripts/prep_deploy.sh
@@ -2,9 +2,19 @@
 
 # Run with `bash scripts/prep_deploy.sh`, paths are relative to the repo root
 
-# Run the commands
+# Export the library
+echo "Exporting library..."
 uv run nbdev_export 
+
+# Build the website
+echo "Building website..."
 uv run nbdev_docs
-bash scripts/prep_colab_nbs.sh
-# git add . 
-# git commit -m "Update site" && git push
+
+# Convert the tutorial notebooks to ipynb
+echo "Converting tutorial notebooks to ipynb..."
+uv run nbdev_qmd_to_ipynb nbs/tutorial _tutorial_ipynbs --copy_other_files False
+
+echo "Done. Now you can run the following commands on the 'main' branch to deploy:"
+echo ""
+echo "    git add . && git commit -m \"Update site\" && git push"
+echo ""
