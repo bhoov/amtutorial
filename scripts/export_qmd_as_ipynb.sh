c378d787e9482c750946d4edbd33e21a3bb7f1f5
Ben Hoover <24350185+bhoov@users.noreply.github.com>
2025-07-10 11:32:25 -0400

    Update desc of memory notebook

--- scripts/export_qmd_as_ipynb.sh
+++ scripts/export_qmd_as_ipynb.sh
@@ -5,7 +5,4 @@ uv run nbdev_qmd_to_ipynb nbs/tutorial tutorial_ipynbs --copy_other_files False
 mkdir -p tutorial_ipynbs/assets/
 cp -r nbs/tutorial/assets/* tutorial_ipynbs/assets/
 
-echo "Tutorials processed. To see changes on colab, run the following commands on the 'main' branch:"
-echo ""
-echo "    git add . && git commit -m \"Update site\" && git push"
-echo ""
+echo "Tutorials processed. Push to main to see changes on colab
